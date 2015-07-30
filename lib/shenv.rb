823f4e924fd14a89b3f722a77493038d4d6fa160
bmd <brian@murphydye.com>
2015-07-30 17:26:47 -0600

    -am Pass test.

--- lib/shenv.rb
+++ lib/shenv.rb
@@ -1,5 +1,16 @@
 require "shenv/version"
 
 module Shenv
-  # Your code goes here...
+  module_function
+
+  def env_as_hash
+    hash = {}
+    env_as_array.each{ |key,val| hash[key] = val }
+    hash
+  end
+
+  def env_as_array
+    env_rows = `set`.split("\n")
+    env_rows.collect{ |row| row.split('=') }
+  end
 end
