a213d6e16b6b11cd7937af84f23542c1337e528a
bmd <brian@murphydye.com>
2015-07-30 17:57:16 -0600

    Pass test.

--- lib/shenv.rb
+++ lib/shenv.rb
@@ -11,6 +11,15 @@ module Shenv
 
   def env_as_array
     env_rows = `set`.split("\n")
-    env_rows.collect{ |row| row.split('=', 2) }
+    pairs = env_rows.collect{ |row| row.split('=', 2) }
+    pairs = pairs.select{ |key,val| !key.nil? and !val.nil? }
+    pairs.collect do |key,val|
+      val = if val.size>1 and val[0]=="'" and val[-1]=="'"
+              val[1..-2]
+            else
+              val
+            end
+      [key,val]
+    end
   end
 end
