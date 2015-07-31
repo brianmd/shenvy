1eefd9dbb71148d97bab778cf507607235c688ea
bmd <brian@murphydye.com>
2015-07-30 18:00:26 -0600

    Add failing test to create diff of envs.

--- spec/shenv_spec.rb
+++ spec/shenv_spec.rb
@@ -8,11 +8,21 @@ describe Shenv do
     env = Shenv.env_as_hash
     expect(env[shenv_key]).to eq(shenv_val)
   end
+
   it 'loads existing environment from file with an equals sing in the value' do
     shenv_key = 'this is a shenv test'
     shenv_val = '7 = 6 + 1'
     ENV[shenv_key] = shenv_val
     env = Shenv.env_as_hash
     expect(env[shenv_key]).to eq(shenv_val)
+  end
+
+  it 'get changes from two environments' do
+    env_before = Shenv.env_as_hash
+    shenv_key = 'this is a shenv test'
+    shenv_val = '7 = 6 + 1'
+    env_after = Shenv.env_as_hash
+    diff = Shenv.env_diff(env_before, env_after)
+    expect(diff).to eq([[shenv_key, shenv_val]])
   end
 end
